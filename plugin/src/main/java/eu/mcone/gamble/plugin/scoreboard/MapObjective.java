082fbf61b326c9767d3e0b99eefb2405a516c734
Dominik Lippl <dominik.lippl@hotmail.de>
2020-07-18 16:36:01 +0200

    Implemented latest BCS and GameSystem, fixed Scoreboards.

--- plugin/src/main/java/eu/mcone/gamble/plugin/scoreboard/MapObjective.java
+++ plugin/src/main/java/eu/mcone/gamble/plugin/scoreboard/MapObjective.java
@@ -7,8 +7,13 @@ package eu.mcone.gamble.plugin.scoreboard;
 
 import eu.mcone.coresystem.api.bukkit.player.CorePlayer;
 import eu.mcone.coresystem.api.bukkit.scoreboard.CoreSidebarObjective;
+import eu.mcone.coresystem.api.bukkit.scoreboard.CoreSidebarObjectiveEntry;
 import eu.mcone.gamble.api.player.GamblePlayer;
 import eu.mcone.gamble.plugin.Gamble;
+import eu.mcone.gameapi.api.GamePlugin;
+import eu.mcone.gameapi.api.Module;
+import eu.mcone.gameapi.api.gamestate.GameState;
+import eu.mcone.gameapi.api.gamestate.common.InGameState;
 import eu.mcone.gameapi.api.scoreboard.InGameObjective;
 
 public class MapObjective extends InGameObjective {
@@ -16,22 +21,22 @@ public class MapObjective extends InGameObjective {
     }
 
     @Override
-    protected void onRegister(CorePlayer corePlayer) {
+    protected void onInGameRegister(CorePlayer corePlayer, CoreSidebarObjectiveEntry entry) {
         setDisplayName("    §6§lGamble");
-        setScore(9, "");
-        setScore(8, "§8» §7Dein Feld:");
-        setScore(6, "");
-        setScore(5, "§8» §7Aktueller Platz:");
-        setScore(3, "");
-        setScore(2, "§8» §7Felder bis zum Ziel:");
-        onReload(corePlayer);
+        entry.setScore(9, "");
+        entry.setScore(8, "§8» §7Dein Feld:");
+        entry.setScore(6, "");
+        entry.setScore(5, "§8» §7Aktueller Platz:");
+        entry.setScore(3, "");
+        entry.setScore(2, "§8» §7Felder bis zum Ziel:");
+        onReload(corePlayer, entry);
     }
 
     @Override
-    protected void onReload(CorePlayer corePlayer) {
+    protected void onInGameReload(CorePlayer corePlayer, CoreSidebarObjectiveEntry entry) {
         GamblePlayer gp = Gamble.getInstance().getGamblePlayer(corePlayer.getUuid());
-        setScore(7, "§f  " + gp.getCurrentPosition() + "/" + Gamble.getInstance().getGameFields());
-        setScore(4, "§f  " + gp.getCurrentPlacing());
-        setScore(1, "§f  " + (Gamble.getInstance().getGameFields() - gp.getCurrentPosition()));
+        entry.setScore(7, "§f  " + gp.getCurrentPosition() + "/" + Gamble.getInstance().getGameFields());
+        entry.setScore(4, "§f  " + gp.getCurrentPlacing());
+        entry.setScore(1, "§f  " + (Gamble.getInstance().getGameFields() - gp.getCurrentPosition()));
     }
 }
